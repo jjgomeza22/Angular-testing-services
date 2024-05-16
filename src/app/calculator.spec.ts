3773d645756c934254d867077d88e8176a42030e
juan_gomez9905 <juan.gomez@bebolder.co>
2024-05-15 22:11:21 -0500

    exploring matchers

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -10,4 +10,33 @@ describe('Test for Calculator', () => {
     //Assert
     expect(rta).toEqual(9);
   });
+
+  it('#multiply should be a four', () => {
+    const calculator = new Calculator();
+    const rta = calculator.multiply(1, 4);
+    expect(rta).toEqual(4);
+  });
+
+  it('#divide should be a some numbers', () => {
+    const calculator = new Calculator();
+    expect(calculator.divide(6, 3)).toEqual(2);
+    expect(calculator.divide(5, 2)).toEqual(2.5);
+  });
+
+  it('#test matchers', () => {
+    const name = 'Juan';
+    let name2;
+
+    expect(name).toBeDefined();
+    expect(name2).toBeUndefined();
+
+    expect(1 + 3 === 4).toBeTruthy();
+    expect(1 + 1 === 3).toBeFalsy();
+
+    expect(5).toBeLessThan(10);
+    expect(20).toBeGreaterThan(10);
+
+    expect('123456').toMatch(/123/);
+    expect(['apples', 'oranges', 'pears']).toContain('oranges');
+  });
 });
