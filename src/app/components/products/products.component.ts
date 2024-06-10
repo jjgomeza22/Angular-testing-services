69288eb3b9897cfe619ab6828b0f3e93b72aa0db
juan_gomez9905 <juan.gomez@bebolder.co>
2024-06-10 16:14:30 -0500

    adding enviroment files and conecting app to API

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../models/product.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  products: Product[] = [];
 
+  private productsService = inject(ProductsService)
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.productsService.getAllSimple()
+    .subscribe(products => {
+      this.products = products;
+    });
+  }
 }
