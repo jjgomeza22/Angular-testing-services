5a0487b3a658ae6d0748b642e79d573d3119b26d
juan_gomez9905 <juan.gomez@bebolder.co>
2024-06-10 15:41:05 -0500

    addin routing to the app and pico css to get semantic styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    loadComponent: () => import('./components/products/products.component').then(m => m.ProductsComponent)
+  },
+  {
+      path: 'pico-preview',
+      loadComponent: () => import('./components/pico-preview/pico-preview.component')
+  },
+];
